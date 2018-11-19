8d81c4ef6b958796d3b2eb087a79cf1c45ae3f16
Lwep3 <44917721+Lwep3@users.noreply.github.com>
2018-11-19 10:52:19 +0800

    我的模块

---
+++ space/src/main/java/com/november/space/param/SpaceParam.java
@@ -0,0 +1,33 @@
+package com.november.space.param;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+public class SpaceParam {
+
+    private Integer id;
+
+    @NotBlank(message = "空间名称不可以为空")
+    @Length(min = 2, max = 20, message = "空间名称长度需要在2-20个字之间")
+    private String spaceName;
+
+    private Integer parentId=-1;
+
+    @Min(value = 0, message = "层级不合法")
+    @Max(value = 2, message = "层级不合法")
+    private String level = "1";
+
+
+    @Length(min = 0, max = 200, message = "空间备注长度需要在200个字符以内")
+    private String remark;
+}
