e78fcf14ca1255f0e0ab33f00f9fd65817fa943d
hahaqiqi <2546082422@qq.com>
2018-12-07 16:56:47 +0800

    fix

--- space/src/main/java/com/november/space/service/SpaceServiceimpl.java
+++ space/src/main/java/com/november/space/service/SpaceServiceimpl.java
@@ -28,57 +28,57 @@ public class SpaceServiceimpl implements SpaceService {
     /*
     判断父空间下的子空间名称
      */
-    public boolean parentIdJudge(Integer parentid,String parentName){
-        return spacemapper.selectByparentid(parentid,parentName)>0;
+    public boolean parentIdJudge(Integer parentid, String parentName) {
+        return spacemapper.selectByparentid(parentid, parentName) > 0;
     }
 
     //增加父空间
-    public int insert(SpaceParam param){
+    public int insert(SpaceParam param) {
         ////检查param类BeanValidator.check
         BeanValidator.check(param);
         //使用parentIdJudge判断父空间下的子空间名称
-       if(parentIdJudge(param.getParentId(),param.getSpaceName())){
-           throw new ParamException("这个父空间已经有这个子空间了");
-       }
+        if (parentIdJudge(param.getParentId(), param.getSpaceName())) {
+            throw new ParamException("这个父空间已经有这个子空间了");
+        }
         //非空判断
-        if(param.getRemark()==""){
+        if (param.getRemark() == "") {
             param.setRemark("暂时没有");
         }
         //使用builder()方法把SpaceParam类存储的数据传递给Space类
-        Space space =Space.builder().spaceName(param.getSpaceName()).level(param.getLevel())
+        Space space = Space.builder().spaceName(param.getSpaceName()).level(param.getLevel())
                 .remark(param.getRemark()).parentId(param.getParentId()).build();
-       if(RequestHolder.getCurrentAdmin()!=null){
-           space.setOperator(RequestHolder.getCurrentAdmin().getAdminCode());
-       }else{
-           space.setOperator("admin");
-       }
-       space.setOperateTime(new Date());
-       return spacemapper.insert(space);
+        if (RequestHolder.getCurrentAdmin() != null) {
+            space.setOperator(RequestHolder.getCurrentAdmin().getAdminCode());
+        } else {
+            space.setOperator("admin");
+        }
+        space.setOperateTime(new Date());
+        return spacemapper.insert(space);
     }
 
     /*
         查询
      */
-    public List<Space> selectList(){
+    public List<Space> selectList() {
         //查询ParentId值为0的父空间
-        List<Space> listSpace=spacemapper.selectList(0);
+        List<Space> listSpace = spacemapper.selectList(0);
         //调用forListSpace方法并把值放入listSpace中
-        listSpace= forListSpace(0,listSpace.size(),listSpace);
+        listSpace = forListSpace(0, listSpace.size(), listSpace);
         return listSpace;
     }
 
-    public List<Space> forListSpace(int start,int end,List<Space> listSpace){
+    public List<Space> forListSpace(int start, int end, List<Space> listSpace) {
         //循环获取ID值
-            for(int i=start;i<end;i++){
-                List<Space> newList=spacemapper.selectList(listSpace.get(i).getId());
-                for (Space space : newList) {
-                    listSpace.add(space);
-                }
+        for (int i = start; i < end; i++) {
+            List<Space> newList = spacemapper.selectList(listSpace.get(i).getId());
+            for (Space space : newList) {
+                listSpace.add(space);
             }
-            if(end!=listSpace.size()){
-                forListSpace(end,listSpace.size(),listSpace);
-            }
-            return listSpace;
+        }
+        if (end != listSpace.size()) {
+            forListSpace(end, listSpace.size(), listSpace);
+        }
+        return listSpace;
     }
 
     /*public List<Space> getListSpaceId(int start,int end,List<Space> listSpace){
@@ -91,23 +91,23 @@ public class SpaceServiceimpl implements SpaceService {
     /*
         修改
      */
-    public int updateByPrimaryKeySelective(SpaceParam param){
+    public int updateByPrimaryKeySelective(SpaceParam param) {
         //检查param类BeanValidator.check
         BeanValidator.check(param);
         //使用parentIdJudge判断父空间下的子空间名称
-        if(parentIdJudge(param.getParentId(),param.getSpaceName())){
+        if (parentIdJudge(param.getParentId(), param.getSpaceName())) {
             throw new ParamException("同一个父空间不能有两个相同的子空间名称");
         }
         //非空判断
-        if(param.getRemark()==""){
+        if (param.getRemark() == "") {
             param.setRemark("暂时没有");
         }
         //使用builder()方法把SpaceParam类存储的数据传递给Space类
-        Space space=Space.builder().id(param.getId()).spaceName(param.getSpaceName()).level(param.getLevel()).
+        Space space = Space.builder().id(param.getId()).spaceName(param.getSpaceName()).level(param.getLevel()).
                 remark(param.getRemark()).build();
-        if(RequestHolder.getCurrentAdmin()!=null){
+        if (RequestHolder.getCurrentAdmin() != null) {
             space.setOperator(RequestHolder.getCurrentAdmin().getAdminCode());
-        }else{
+        } else {
             space.setOperator("admin");
         }
         space.setOperateTime(new Date());
@@ -118,12 +118,12 @@ public class SpaceServiceimpl implements SpaceService {
     /*
         删除
      */
-    public int deleteByPrimaryKey(Integer id){
-        List<Space> listSpace=spacemapper.selectList(id);
-        listSpace=forListSpace(0,listSpace.size(),listSpace);
-        List<Integer> listId=new ArrayList<>();
+    public int deleteByPrimaryKey(Integer id) {
+        List<Space> listSpace = spacemapper.selectList(id);
+        listSpace = forListSpace(0, listSpace.size(), listSpace);
+        List<Integer> listId = new ArrayList<>();
         listId.add(id);
-        for(Space sp :listSpace){
+        for (Space sp : listSpace) {
             listId.add(sp.getId());
         }
         return spacemapper.deleteByPrimaryKey(listId);
@@ -132,25 +132,26 @@ public class SpaceServiceimpl implements SpaceService {
         20 23 24 25 32 33 34 35 36 37
      */
 
-    public int Movespace(Integer id,Integer pid){
+    public int Movespace(Integer id, Integer pid) {
         //使用parentIdJudge判断父空间下的子空间名称
-        Space param=spacemapper.selectByPrimaryKey(id);
-        if(parentIdJudge(pid,param.getSpaceName())){
+        Space param = spacemapper.selectByPrimaryKey(id);
+        if (parentIdJudge(pid, param.getSpaceName())) {
             throw new ParamException("同一个父空间不能有两个相同的子空间名称");
         }
-        return spacemapper.Movespace(id,pid);
+        return spacemapper.Movespace(id, pid);
     }
 
-    public List<Integer> selectSpaceBook(Integer id){
-        List<Space> listSpace=spacemapper.selectList(id);
-        listSpace=forListSpace(0,listSpace.size(),listSpace);
-        List<Integer> listId=new ArrayList<>();
+    public List<Integer> selectSpaceBook(Integer id) {
+        List<Space> listSpace = spacemapper.selectList(id);
+        listSpace = forListSpace(0, listSpace.size(), listSpace);
+        List<Integer> listId = new ArrayList<>();
         listId.add(id);
-        for(Space sp :listSpace){
+        for (Space sp : listSpace) {
             listId.add(sp.getId());
         }
         return listId;
     }
+
     /*@Override
     public int updateByPrimaryKey(SpaceParam record) {
         return spacemapper.updateByPrimaryKey(record);
